335dc32bb9f73ca06b843b426904c6a36c05f438
Jesse Glick <jglick@cloudbees.com>
2018-01-03 13:49:49 -0500

    Avoid builder syntax.

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -1,8 +1,4 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+def call(Map config) {
     node {
         stage('Checkout') {
             checkout scm
